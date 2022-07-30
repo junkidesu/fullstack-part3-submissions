6823e45e87fffacc6bbf645a898c838c01b9f992
Anwar Sh <alvaro.sh03@gmail.com>
2022-07-30 19:10:07 +0500

    Configure CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
