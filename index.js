d5a497533939b3f75dff41c64f30cae7aff1b171
Anwar Sh <alvaro.sh03@gmail.com>
2022-07-30 19:33:55 +0500

    Add frontend production build

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
