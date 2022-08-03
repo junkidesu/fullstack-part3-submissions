6161cb2aeed822ae16ceae0fd2767faea9194ff8
Anwar Sh <alvaro.sh03@gmail.com>
2022-08-03 20:41:51 +0500

    Connect all routes to MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
 const app = express()
+
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.static('build'))
@@ -41,63 +45,87 @@ let persons = [
 ]
 
 app.get('/info', (request, response) => {
-    response.send(`
-    <div>
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date().toUTCString()}</p>
-    </div>
-    `)
+    Person.find({})
+        .then(persons => {
+            response.send((
+                `
+                <div>
+                    <p>Phonebook has info for ${persons.length} people</p>
+                    <p>${new Date().toUTCString()}</p>
+                </div>
+                `
+            ))
+        })
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person
+        .find({})
+        .then(persons => {
+            response.json(persons)
+        })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
-    const id = Math.floor(Math.random() * 100000)
+    const body = request.body
 
-    const person = { ...request.body }
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    })
 
-    if (!person.name || !person.number) {
-        return response.status(400).json(
-            {
-                error: "name or number missing"
-            }
-        )
-    }
-
-    if (persons.some(p => p.name === person.name)) {
-        return response.status(400).json(
-            {
-                error: "name must be unique"
-            }
-        )
-    }
-
-    person.id = id
-
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save()
+        .then(addedPerson => {
+            response.json(addedPerson)
+        })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 
